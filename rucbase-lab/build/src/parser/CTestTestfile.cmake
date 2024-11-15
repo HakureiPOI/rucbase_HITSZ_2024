b80841d8361f2af988d3a26ebcdf74f7593fef6e
HakureiPOI <hakureipoi@qq.com>
2024-11-15 06:26:39 +0000

    Initial Commit

---
+++ rucbase-lab/build/src/parser/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /content/rucbase-lab/src/parser
+# Build directory: /content/rucbase-lab/build/src/parser
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_parser "/content/rucbase-lab/build/bin/test_parser")
+set_tests_properties(test_parser PROPERTIES  WORKING_DIRECTORY "/content/rucbase-lab/build/bin" _BACKTRACE_TRIPLES "/content/rucbase-lab/src/parser/CMakeLists.txt;14;add_test;/content/rucbase-lab/src/parser/CMakeLists.txt;0;")
---
+++ rucbase-lab/deps/googletest/build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /content/rucbase-lab/deps/googletest/googlemock
+# Build directory: /content/rucbase-lab/deps/googletest/build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
